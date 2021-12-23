1c09551970dd70d710d5061949cc6efb4d9171ab
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2021-12-23 18:42:55 -0500

    Add project files.

---
+++ Comp100 Lesson4/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool condiction = true;
+while (condiction == true) 
+{
+    Console.WriteLine("1 for pizza 2 for burger 3 for nothing");
+    int response = Convert.ToInt32(Console.ReadLine());
+    switch (response) 
+    {
+        case 1:
+        case 2:
+            Console.WriteLine("you are eating a burger or pizza");
+            break;
+        case 3:
+            condiction = false;
+            break;
+        default:
+            Console.WriteLine("invalid input");
+            break ;
+    }
+}
+
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
